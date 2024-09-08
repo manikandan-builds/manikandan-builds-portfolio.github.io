ac435d688acba7338b51a7e5ceed852a8b9ae51f
Manin02 <manikandan2112a18@gmail.com>
2024-09-08 11:39:58 +0530

    first

--- controller.js
+++ controller.js
@@ -1,11 +1,8 @@
 window.onload = () => {
-    const navMenu = document.querySelector('.nav-menu');
-    const navItems = document.querySelectorAll('.nav-item');
-    const hamburger = document.querySelector('.nav-toggle');
-  
-    const toggle = e => e.classList.toggle('is-active');
-    
-    hamburger.addEventListener('click', () => toggle(navMenu));
-    navItems.forEach(item => item.addEventListener('click', () => navMenu.classList.remove('is-active')));
-  }
-  
+  const navMenu = document.querySelector('.nav-menu');
+  const hamburger = document.querySelector('.nav-toggle');
+
+  hamburger.addEventListener('click', () => {
+      navMenu.classList.toggle('is-active');
+  });
+};
